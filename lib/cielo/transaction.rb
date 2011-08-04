29351b1ebd8d46d206e792cbf1d01e3b54730147
Gabriel Paladino <gabriel@scupen.com>
2011-08-04 14:41:14 -0300

    deixando funcional 1.8.7

--- lib/cielo/transaction.rb
+++ lib/cielo/transaction.rb
@@ -1,48 +1,88 @@
 #encoding: utf-8
 module Cielo
   class Transaction
+    
     def initialize
       @connection = Cielo::Connection.new
     end
+    
+    # inicia uma nova transação para processamento Buy Page Cielo
     def create!(parameters={})
-      analysis_parameters(parameters)
+      create_parameters(parameters)
       message = xml_builder("requisicao-transacao") do |xml|
-        xml.send("dados-pedido") do
+        xml.tag!("dados-pedido") do
           [:numero, :valor, :moeda, :"data-hora", :idioma].each do |key|
-            xml.send(key.to_s, parameters[key].to_s)
+            xml.tag!(key.to_s, parameters[key].to_s)
           end
         end
-        xml.send("forma-pagamento") do
+        xml.tag!("forma-pagamento") do
           [:bandeira, :produto, :parcelas].each do |key|
-            xml.send(key.to_s, parameters[key].to_s)
+            xml.tag!(key.to_s, parameters[key].to_s)
           end
         end
-        xml.send("url-retorno", parameters[:"url-retorno"])
+        xml.tag!("url-retorno", parameters[:"url-retorno"])
         xml.autorizar parameters[:autorizar].to_s
         xml.capturar parameters[:capturar].to_s
       end
       make_request! message
     end
     
-    def verify!(cielo_tid)
-      return nil unless cielo_tid
-      message = xml_builder("requisicao-consulta", :before) do |xml|
-        xml.tid "#{cielo_tid}"
+    # requisição do TID, inicia a transação para efetuar uma autorização direta - Buy Page Loja
+    def request_tid!(parameters={})
+      message = xml_builder("requisicao-tid") do |xml|
+        xml.tag!("forma-pagamento") do
+          [:bandeira, :produto, :parcelas].each do |key|
+            xml.tag!(key.to_s, parameters[key].to_s)
+          end
+        end
       end
-      
       make_request! message
     end
     
+    # efetua a autorização direta passando todos os dados da transação - Buy Page Loja
+    def direct_auth!(parameters={})
+      auth_parameters(parameters)
+      message = xml_builder("requisicao-autorizacao-portador") do |xml|
+        xml.tid parameters[:tid]
+        xml.tag!("dados-pedido") do
+          [:numero, :valor, :moeda, :"data-hora", :idioma].each do |key|
+            xml.tag!(key.to_s, parameters[key].to_s)
+          end
+        end
+        xml.tag!("forma-pagamento") do
+          [:bandeira, :produto, :parcelas].each do |key|
+            xml.tag!(key.to_s, parameters[key].to_s)
+          end
+        end
+        xml.tag!("url-retorno", parameters[:"url-retorno"])
+        xml.autorizar parameters[:autorizar].to_s
+        xml.capturar parameters[:capturar].to_s
+      end
+      make_request! message
+    end
+    
+    # efetua uma consulta e obtem os dados da transação
+    def verify!(cielo_tid)
+      return nil unless cielo_tid
+      message = xml_builder("requisicao-consulta") do |xml|
+        xml.tid "#{cielo_tid}"
+      end
+      make_request! message
+    end
+    
+    # captura uma transação que já esteja autorizada
     def catch!(cielo_tid)
       return nil unless cielo_tid
-      message = xml_builder("requisicao-captura", :before) do |xml|
+      message = xml_builder("requisicao-captura") do |xml|
         xml.tid "#{cielo_tid}"
       end
       make_request! message
     end
     
     private
-    def analysis_parameters(parameters={})
+    
+    # verifica os parametros para iniciar uma nova transação
+    def create_parameters(parameters={})
       [:numero, :valor, :bandeira].each do |parameter|
         raise Cielo::MissingArgumentError, "Required parameter #{parameter} not found" unless parameters[parameter]
       end
@@ -57,27 +97,56 @@ module Cielo
       parameters
     end
     
-    def xml_builder(group_name, target=:after, &block)
-      xml = Builder::XmlMarkup.new
+    # verifica os parametros para efetuar uma autorização direta
+    def auth_parameters(parameters={})
+      create_parameters(parameters)
+      [:numero, :valor, :bandeira].each do |parameter|
+        raise Cielo::MissingArgumentError, "Required parameter #{parameter} not found" unless parameters[parameter]
+      end
+      parameters.merge!(:moeda => "986") unless parameters[:moeda]
+      parameters.merge!(:"data-hora" => Time.now.strftime("%Y-%m-%dT%H:%M:%S")) unless parameters[:"data-hora"]
+      parameters.merge!(:idioma => "PT") unless parameters[:idioma]
+      parameters.merge!(:produto => "1") unless parameters[:produto]
+      parameters.merge!(:parcelas => "1") unless parameters[:parcelas]
+      parameters.merge!(:autorizar => "2") unless parameters[:autorizar]
+      parameters.merge!(:capturar => "true") unless parameters[:capturar]
+      parameters.merge!(:"url-retorno" => Cielo.return_path) unless parameters[:"url-retorno"]
+      parameters
+    end
+    
+    # gera xml para as requisições ao webservice
+    def xml_builder(group_name, &block)
+      xml = Builder::XmlMarkup.new(:indent => 2)
       xml.instruct! :xml, :version => "1.0", :encoding => "ISO-8859-1"
-      xml.send(group_name, :id => "#{Time.now.to_i}", :versao => "1.1.0") do
-        block.call(xml) if target == :before
-        xml.send("dados-ec") do
+      xml.tag!(group_name, :id => "#{Time.now.to_i}", :versao => "1.1.0") do
+      #xml.send(group_name, :id => "#{Time.now.to_i}", :versao => "1.1.0") do
+        #block.call(xml) if target == :before
+        
+        xml.tag!("dados-ec") do
           xml.numero Cielo.numero_afiliacao
           xml.chave Cielo.chave_acesso
         end
-        block.call(xml) if target == :after
-      end
+        
+        #xml.send("dados-ec") do
+        #  xml.numero Cielo.numero_afiliacao
+        #  xml.chave Cielo.chave_acesso
+        #end
+        block.call(xml) # if target == :after
+      #end
+      end 
       xml
     end
     
+    # efetua requisição ao webservice
     def make_request!(message)
+      #message.target!
       params = { :mensagem => message.target! }
       
       result = @connection.request! params
       parse_response(result)
     end
     
+    # obtém o retorno do webservice em XML
     def parse_response(response)
       case response
       when Net::HTTPSuccess
@@ -87,6 +156,8 @@ module Cielo
         {:erro => { :codigo => "000", :mensagem => "Impossível contactar o servidor"}}
       end
     end
+    
+    # trata o retorno do webservice tranformando o XML em Hash
     def parse_elements(elements)
       map={}
       elements.each do |element|
@@ -98,5 +169,6 @@ module Cielo
       end
       map.symbolize_keys
     end
+    
   end
 end
